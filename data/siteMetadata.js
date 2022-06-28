0438c1202cbc0e78fa0016539038bbe4fa91bf3d
malekpouri <m.malekpoor@gmail.com>
2022-06-28 10:09:25 +0430

    update site meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,17 +5,14 @@ const siteMetadata = {
   description: 'محلی برای به اشتراک گذاری یک صفحه از زندگی کاری',
   language: 'fa-ir',
   theme: 'system', // system, dark or light
-  siteUrl: 'http://localhost:3000',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://gheseto.ir',
+  siteRepo: 'https://github.com/malekpouri/ghesetoo',
   siteLogo: '/static/images/logo.jpeg',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'admin@utux.ir',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  github: 'https://github.com/malekpouri/',
+  twitter: 'https://twitter.com/m_malekpouri',
   locale: 'fa-IR',
   analytics: {
     googleAnalyticsId: 'G-Z548ZF0X8H', // e.g. UA-000000-2 or G-XXXXXXX
