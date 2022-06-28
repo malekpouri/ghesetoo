483f1da095223c84267ddb5ed4c6a0c1781bb02d
malekpouri <m.malekpoor@gmail.com>
2022-06-28 14:20:40 +0430

    comment GoogleAnalytics.js

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,27 +1,27 @@
-import siteMetadata from "@/data/siteMetadata";
-
-const GoogleAnalytics = () => {
-  return (
-      <>
-          {/*Global site tag (gtag.js) - Google Analytics */}
-          <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
-          />
-          <script
-              dangerouslySetInnerHTML={{
-                  __html: `
-                                    window.dataLayer = window.dataLayer || [];
-                                    function gtag(){dataLayer.push(arguments);}
-                                    gtag('js', new Date());
-                                    gtag('config', '${siteMetadata.analytics.googleAnalyticsId}', {
-                                page_path: window.location.pathname,
-                                });
-                                `,
-              }}
-          />
-      </>
-  )
-}
-
-export default GoogleAnalytics
+// import siteMetadata from "@/data/siteMetadata";
+//
+// const GoogleAnalytics = () => {
+//   return (
+//       <>
+//           {/*Global site tag (gtag.js) - Google Analytics */}
+//           <script
+//               async
+//               src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
+//           />
+//           <script
+//               dangerouslySetInnerHTML={{
+//                   __html: `
+//                                     window.dataLayer = window.dataLayer || [];
+//                                     function gtag(){dataLayer.push(arguments);}
+//                                     gtag('js', new Date());
+//                                     gtag('config', '${siteMetadata.analytics.googleAnalyticsId}', {
+//                                 page_path: window.location.pathname,
+//                                 });
+//                                 `,
+//               }}
+//           />
+//       </>
+//   )
+// }
+//
+// export default GoogleAnalytics
