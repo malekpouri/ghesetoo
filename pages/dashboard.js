31a91b20cbeb44d80a46067775b79dc2b98a0280
malekpouri <m.malekpoor@gmail.com>
2022-08-20 14:17:41 +0430

    fix bug setViewMail

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -18,9 +18,9 @@ function dashboard() {
   const [saveProfile, setSaveProfile] = useState(false);
   const { data: session } = useSession()
 
-  if (!session) {
-    return <AccessDenied />
-  }
+  // if (!session) {
+  //   return <AccessDenied />
+  // }
   const showModalClicked = () => {
     setModalOn(true)
   }
@@ -49,11 +49,13 @@ useEffect( ()=>{
   getData()
 },[])
   useEffect(() => {
+
 if (saveProfile) {
       const timeout = setTimeout(() => { setSaveProfile(false)}, 3000);
     }
   }, [saveProfile])
   const saveInfoHandler =async () => {
+    console.log(viewMail)
     const user={
       id: userId,
       email:email,
@@ -197,10 +199,9 @@ if (saveProfile) {
           <div className="form-check m-5">
             <input
                 className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="checkbox" value="" id="flexCheckDefault" />
-              <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault"
-              checked={viewMail}
-              onChange={() => setViewMail(!viewMail)}>
+                type="checkbox" value="" id="flexCheckDefault"  checked={viewMail}
+                onChange={() => setViewMail(!viewMail)}/>
+              <label className="form-check-label inline-block text-gray-800" htmlFor="flexCheckDefault">
                 نمایش ایمیل برای دیگران
               </label>
           </div>
